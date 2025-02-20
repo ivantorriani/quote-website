a0565e38e58fb3379e5a5672ee97ca4ee0649c49
Ivan Torriani <150189391+ivantorriani@users.noreply.github.com>
2025-02-20 15:00:45 -0800

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import styles from './page.module.css';
 import BookSchema from './database/bookschema';
-import QuoteSchema from './database/quoteschema';
+//import QuoteSchema from './database/quoteschema';
 import Image from 'next/image';
 import Link from 'next/link';
 import { format } from "date-fns";
@@ -46,7 +46,7 @@ export default function Page() {
       <div className = {styles.quoteContainer}>
           <p className = {styles.quotetheday}> Imagine if each day a man must try to kill the moon, he thought. The moon runs away. But imagine if a man each day should have to try to kill the sun? We were born lucky, he thought</p>
           <p className = {styles.author}>  Santiago (The Old Man), The Old Man and the Sea, Ernest Hemingway</p>
-          <p className = {styles.description}> Santiago hasn't caught a fish in years. But he remembers that, unlike killing the moon or the sun, his goal to catch a fish is achievable. We should be grateful that most of our problems are within our control.</p>
+          <p className = {styles.description}> Santiago has not caught a fish in years. But he remembers that, unlike killing the moon or the sun, his goal to catch a fish is achievable. We should be grateful that most of our problems are within our control.</p>
       </div>
       <div className = {styles.slide2}>
         <p>Interested in more quotes? Check out </p> <Link href="/quote-archive"> the quote archive </Link> 
