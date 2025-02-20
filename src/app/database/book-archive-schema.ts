6e7b25ce09f12aee5d6304f2b9c327a86f5bff68
Ivan Torriani <150189391+ivantorriani@users.noreply.github.com>
2025-02-20 10:56:20 -0800

    update

--- src/app/database/book-archive-schema.ts
+++ src/app/database/book-archive-schema.ts
@@ -1,7 +1,7 @@
 const ArchiveSchema = [
     { title: "Diplomacy", author: "Henry Kissinger", image: "/book-pics/diplomacy.png" },
     { title: "The Little Prince", author: "Antoine de Saint-Exupéry", image: "/book-pics/thelittleprince.png" },
-    { title: "Invisible Cities", author: "Italo Calvino", image: "/book-pics/invisible-cities.png" },
+    { title: "Lonesome Dove", author: "Larry McMurty", image: "/book-pics/lonesomedove.jpg" },
   ];
   
   export default ArchiveSchema;
--- src/app/database/bookschema.ts
+++ src/app/database/bookschema.ts
@@ -1,7 +1,7 @@
 const BookSchema = [
   { title: "Diplomacy", author: "Henry Kissinger", image: "/book-pics/diplomacy.png" },
   { title: "The Old Man and the Sea", author: "Ernest Hemingway ", image: "/book-pics/oldmanandsea.jpg" },
-  { title: "Invisible Cities", author: "Italo Calvino", image: "/book-pics/invisible-cities.png" },
+  { title: "Lonesome Dove", author: "Larry McMurty", image: "/book-pics/lonesomedove.jpg" },
 ];
 
 export default BookSchema;
